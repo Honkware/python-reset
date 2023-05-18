d21888f35e0ccca4947e8019554539fd7305f253
Honkware <119620994+Honkware@users.noreply.github.com>
2023-05-17 23:49:46 -0500

    suppress errors

--- PythonReset.ps1
+++ PythonReset.ps1
@@ -12,20 +12,20 @@ foreach ($uninstallKey in $uninstallKeys) {
 }
 
 # Remove Python directories manually
-$pythonDirectories = Get-ChildItem -Path "C:\", "D:\", "E:\" -Directory -Recurse |
+$pythonDirectories = Get-ChildItem -Path "C:\", "D:\", "E:\" -Directory -Recurse -Exclude "C:\Program Files\Windows Defender Advanced Threat Protection", "C:\Windows\CSC", "C:\Windows\System32\LogFiles\WMI\RtBackup" -ErrorAction SilentlyContinue |
     Where-Object { $_.Name -match "Python" -and $_.Name -notlike "*Python27-x64*" }
 
 foreach ($directory in $pythonDirectories) {
     Write-Host "Removing $($directory.FullName)" -ForegroundColor Cyan
-    Remove-Item -Path $directory.FullName -Recurse -Force
+    Remove-Item -Path $directory.FullName -Recurse -Force -ErrorAction SilentlyContinue
 }
 
 # Find and remove pip and related files
-$pipFiles = Get-ChildItem -Path "C:\", "D:\", "E:\" -Include "pip*", "easy_install*", "wheel" -File -Recurse
+$pipFiles = Get-ChildItem -Path "C:\", "D:\", "E:\" -Include "pip*", "easy_install*", "wheel" -File -Recurse -ErrorAction SilentlyContinue
 
 foreach ($file in $pipFiles) {
     Write-Host "Removing $($file.FullName)" -ForegroundColor Cyan
-    Remove-Item -Path $file.FullName -Force
+    Remove-Item -Path $file.FullName -Force -ErrorAction SilentlyContinue
 }
 
 # Clean up environment variables
@@ -48,7 +48,7 @@ foreach ($envVar in $envVarsToRemove) {
 }
 
 # Remove Python-related paths from PATH variable in user profiles
-$userProfiles = Get-ChildItem -Path "C:\Users" -Directory
+$userProfiles = Get-ChildItem -Path "C:\Users" -Directory -ErrorAction SilentlyContinue
 
 foreach ($profile in $userProfiles) {
     $profilePath = Join-Path -Path $profile.FullName -ChildPath ".\AppData\Roaming\Python"
@@ -56,9 +56,9 @@ foreach ($profile in $userProfiles) {
         Write-Host "Removing Python references from PATH in user profile $($profile.Name)" -ForegroundColor Cyan
         $pathFile = Join-Path -Path $profilePath -ChildPath "pth"
         if (Test-Path -Path $pathFile) {
-            $pathLines = Get-Content -Path $pathFile
+            $pathLines = Get-Content -Path $pathFile -ErrorAction SilentlyContinue
             $pathLines = $pathLines | Where-Object { $_ -notlike "*python*" }
-            $pathLines | Set-Content -Path $pathFile
+            $pathLines | Set-Content -Path $pathFile -ErrorAction SilentlyContinue
         }
     }
 }
