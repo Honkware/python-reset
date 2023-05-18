83e1964ef131f6b34d5a48bacac60fedc3ec4a2b
Honkware <119620994+Honkware@users.noreply.github.com>
2023-05-17 23:47:22 -0500

    more comprehensive script

--- PythonReset.ps1
+++ PythonReset.ps1
@@ -1,25 +1,67 @@
-# Uninstall existing Python installations
-Get-ChildItem -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall" -Recurse | 
-ForEach-Object { Get-ItemProperty $_.PSPath } | 
-Where-Object { $_.DisplayName -match "Python" } | 
-ForEach-Object { Start-Process -FilePath "msiexec.exe" -ArgumentList "/x $($_.PSChildName) /qn" -Wait }
+# Find and uninstall Python installations from registry
+$uninstallKeys = Get-ChildItem -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall", "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall" -Recurse |
+    ForEach-Object { Get-ItemProperty $_.PSPath } |
+    Where-Object { $_.DisplayName -match "Python" }
 
-# Download and install the latest Python version
-iwr -Uri "https://www.python.org/downloads/windows/" -OutFile "python-page.html"
-$url = (Select-String -Path "python-page.html" -Pattern 'https:\/\/www\.python\.org\/ftp\/python\/\d+\.\d+\.\d+\/python-\d+\.\d+\.\d+-amd64\.exe' -AllMatches).Matches[0].Value
-iwr -Uri $url -OutFile "python-installer.exe"
-Start-Process -FilePath "python-installer.exe" -ArgumentList "/passive InstallAllUsers=1 PrependPath=1" -Wait
-Remove-Item "python-installer.exe"
-Remove-Item "python-page.html"
-
-# Refresh environment variables in the current session
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-
-# Test Python installation
-$pythonExe = (Get-Command python).Source
-$pythonVersion = & $pythonExe --version 2>&1
-if ($pythonVersion -match "Python") {
-    Write-Host "Python installation successful! Version: $pythonVersion" -ForegroundColor Green
-} else {
-    Write-Host "Python installation failed. Please try again or check the installation manually." -ForegroundColor Red
+foreach ($uninstallKey in $uninstallKeys) {
+    $uninstallString = $uninstallKey.PSChildName
+    if ($uninstallString) {
+        Write-Host "Uninstalling $($uninstallKey.DisplayName)" -ForegroundColor Cyan
+        Start-Process -FilePath "msiexec.exe" -ArgumentList "/x $uninstallString /qn" -Wait
+    }
 }
+
+# Remove Python directories manually
+$pythonDirectories = Get-ChildItem -Path "C:\", "D:\", "E:\" -Directory -Recurse |
+    Where-Object { $_.Name -match "Python" -and $_.Name -notlike "*Python27-x64*" }
+
+foreach ($directory in $pythonDirectories) {
+    Write-Host "Removing $($directory.FullName)" -ForegroundColor Cyan
+    Remove-Item -Path $directory.FullName -Recurse -Force
+}
+
+# Find and remove pip and related files
+$pipFiles = Get-ChildItem -Path "C:\", "D:\", "E:\" -Include "pip*", "easy_install*", "wheel" -File -Recurse
+
+foreach ($file in $pipFiles) {
+    Write-Host "Removing $($file.FullName)" -ForegroundColor Cyan
+    Remove-Item -Path $file.FullName -Force
+}
+
+# Clean up environment variables
+$envVarsToRemove = @(
+    "Path",
+    "PYTHONPATH",
+    "PYTHONSTARTUP",
+    "PYTHONHOME",
+    "PYTHONCASEOK",
+    "PYTHONIOENCODING",
+    "PY_HOME",
+    "PY_PYTHON"
+)
+
+foreach ($envVar in $envVarsToRemove) {
+    $currentValue = [System.Environment]::GetEnvironmentVariable($envVar, "Machine")
+    $newValue = $currentValue -split ";" | Where-Object { $_ -notlike "*python*" } -join ";"
+    [System.Environment]::SetEnvironmentVariable($envVar, $newValue, "Machine")
+    Write-Host "Removed Python references from $envVar" -ForegroundColor Green
+}
+
+# Remove Python-related paths from PATH variable in user profiles
+$userProfiles = Get-ChildItem -Path "C:\Users" -Directory
+
+foreach ($profile in $userProfiles) {
+    $profilePath = Join-Path -Path $profile.FullName -ChildPath ".\AppData\Roaming\Python"
+    if (Test-Path -Path $profilePath) {
+        Write-Host "Removing Python references from PATH in user profile $($profile.Name)" -ForegroundColor Cyan
+        $pathFile = Join-Path -Path $profilePath -ChildPath "pth"
+        if (Test-Path -Path $pathFile) {
+            $pathLines = Get-Content -Path $pathFile
+            $pathLines = $pathLines | Where-Object { $_ -notlike "*python*" }
+            $pathLines | Set-Content -Path $pathFile
+        }
+    }
+}
+
+# Restart computer
+Write-Host "Please restart your computer to complete the uninstallation process." -ForegroundColor Cyan
